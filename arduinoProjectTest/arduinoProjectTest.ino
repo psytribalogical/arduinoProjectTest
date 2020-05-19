99243b25e55c0089a64a081be7a0fe1a93329420
Simon Hopkinson <simonphopkinson@gmail.com>
2020-05-19 14:44:19 +0100

    Initial Project Commit

---
+++ arduinoProjectTest/arduinoProjectTest.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		arduinoProjectTest.ino
+ Created:	5/19/2020 2:42:46 PM
+ Author:	Simon
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() { 
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
